31db83e9f97e777638cd6b2772d8f9b4c68e5f72
Marco Rogers <marco.rogers@gmail.com>
2012-05-27 22:16:20 -0700

    removed unused uuid function

--- couchie.js
+++ couchie.js
@@ -1,9 +1,5 @@
 (function (window) {
 
-  var uuid = function b (a) {
-    return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,b)
-  }
-  
   function Couchie (name) {
     if (name.indexOf('__') !== -1) throw new Error('Cannot have double underscores in name')
     this.name = name
